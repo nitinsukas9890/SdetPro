41769c4f9c639a0e4234f43ce0a29bc360612c51
nitinsukas9890 <118243214+nitinsukas9890@users.noreply.github.com>
2022-11-15 12:05:13 +0530

    Update classs2.java

--- sssss/src/main/java/nsss/classs2.java
+++ sssss/src/main/java/nsss/classs2.java
@@ -3,10 +3,10 @@ package nsss;
 public class classs2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 char a;
-         a= 'r';
-         System.out.println(a);  
+		// TODO Auto-generated method st
+         System.out.println(a); 
+		  System.out.println(a);
+		  System.out.println(a);
 
 	}
 
