f65ccbb9d84c1ebfd3e68ba5c20741b825aba53d
nitin <nitin@192.168.0.111>
2022-11-15 11:47:28 +0530

    committ second

---
+++ sssss/src/main/java/nsss/classs2.java
@@ -0,0 +1,13 @@
+package nsss;
+
+public class classs2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 char a;
+         a= 'r';
+         System.out.println(a);  
+
+	}
+
+}
