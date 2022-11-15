95b719b39cf4cee63a59c13a7324d82c818e9f80
nitin <nitin@192.168.0.111>
2022-11-15 14:28:20 +0530

    commiot second

---
+++ Rep/src/test/java/Repository/class0.java
@@ -0,0 +1,11 @@
+package Repository;
+
+public class class0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+             System.out.println("a"); 
+             System.out.println("b");
+	}
+
+}
